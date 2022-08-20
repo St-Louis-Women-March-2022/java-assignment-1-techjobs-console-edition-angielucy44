d6b15e4db79880618ab4b173d622dfa34142f080
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-08-20 12:28:36 -0500

    checking commit function

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -14,7 +14,7 @@ import java.util.List;
  * Created by LaunchCode
  */
 public class JobData {
-    //TESTING COMMIT FUNCTION AGAIN AGAIN AGAIN again
+
     private static final String DATA_FILE = "src/main/resources/job_data.csv";
     private static boolean isDataLoaded = false;
 
