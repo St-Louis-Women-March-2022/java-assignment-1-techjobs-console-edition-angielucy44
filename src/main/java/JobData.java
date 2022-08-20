66038dc61ed23af5db8411c7e07f0a0bd8fa5765
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-08-20 13:33:05 -0500

    findValue

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -97,9 +94,26 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs){
+
+            for Map.Entry<String, String> column : row.entrySet() {
+
+                String a = column.getKey();
+                String b = column.getKey();
+
+                String a = a.toUpperCase();
+                String b = b.toUpperCase();
+
+                if(a.contains(value) || b.contains(value)){
+                    jobs.add(row);
+                }
+            }
+        }
 
         // TODO - implement this method
-        return null;
+        return jobs;
     }
 
     /**
